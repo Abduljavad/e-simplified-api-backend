23d152e6dac79005489356977273acf55762ae0f
ABDUL JAVAD <javad32@gmail.com>
2023-09-05 15:22:16 +0530

    chore(mocktest):mock test modules added

---
+++ app/Http/Controllers/MockTestController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMockTestRequest;
+use App\Http\Requests\UpdateMockTestRequest;
+use App\Models\MockTest;
+
+class MockTestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMockTestRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMockTestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MockTest  $mockTest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MockTest $mockTest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMockTestRequest  $request
+     * @param  \App\Models\MockTest  $mockTest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMockTestRequest $request, MockTest $mockTest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MockTest  $mockTest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MockTest $mockTest)
+    {
+        //
+    }
+}
